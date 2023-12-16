8c84b43cac73ef641d50a7766c6b6fa8d09b5a56
Tnalxmsk <alstnwkd990@naver.com>
2023-12-16 18:22:03 +0900

    feat(oncall): 근무 순서를 변경하는 기능 추가

--- src/main/kotlin/oncall/utils/WorkScheduleGenerator.kt
+++ src/main/kotlin/oncall/utils/WorkScheduleGenerator.kt
@@ -8,27 +8,29 @@ object WorkScheduleGenerator {
     fun generateSchedule(
         workDate: WorkDate,
         weekDayWorkers: List<Worker>,
-        weekendWorkers: List<Worker>)
-    : List<WorkSchedule> {
+        weekendWorkers: List<Worker>
+    )
+            : List<WorkSchedule> {
         val month = workDate.month
         val schedule = mutableListOf<WorkSchedule>()
         val days = MonthRule.getDays(month)
         var weekdayWorkerIndex = 0
         var weekendWorkerIndex = 0
         var dayOfWeekIndex = DayOfWeek.DAYS.day.indexOf(workDate.firstDayWeek)
+        var worker: Worker
 
         for (i in 1..days) {
-            var worker: Worker
             dayOfWeekIndex = IndexChecker.checkDayOfWeekIndex(dayOfWeekIndex)
             val dayOfWeek = DayOfWeek.DAYS.day[dayOfWeekIndex]
             when {
-                PublicHoliday.isHoliday(month ,i) || Weekend.isWeekend(dayOfWeek) -> {
+                PublicHoliday.isHoliday(month, i) || Weekend.isWeekend(dayOfWeek) -> {
                     weekendWorkerIndex = IndexChecker.checkIndex(weekendWorkerIndex, weekendWorkers)
                     worker = weekendWorkers[weekendWorkerIndex]
                     weekendWorkerIndex++
                     dayOfWeekIndex++
                     schedule.add(WorkSchedule(month, i, dayOfWeek, worker.name))
                 }
+
                 else -> {
                     weekdayWorkerIndex = IndexChecker.checkIndex(weekdayWorkerIndex, weekDayWorkers)
                     worker = weekDayWorkers[weekdayWorkerIndex]
@@ -38,6 +40,53 @@ object WorkScheduleGenerator {
                 }
             }
         }
+        fixSchedule(schedule)
         return schedule
+    }
+
+    private fun fixSchedule(schedule: MutableList<WorkSchedule>) {
+        for (i in schedule.indices) {
+            if (i + 1 == schedule.lastIndex) {
+                return
+            }
+            if (schedule[i].name == schedule[i + 1].name &&
+                (!PublicHoliday.isHoliday(schedule[i].month, schedule[i].day) &&
+                        !Weekend.isWeekend(schedule[i].dayOfWeek))
+            ) {
+                findNextWeekdayWorker(i + 1, schedule)
+            }
+            if (schedule[i].name == schedule[i + 1].name &&
+                (PublicHoliday.isHoliday(schedule[i].month, schedule[i].day) ||
+                        Weekend.isWeekend(schedule[i].dayOfWeek))
+            ) {
+                findNextHolidayWorker(i + 1, schedule)
+            }
+        }
+    }
+
+    private fun findNextHolidayWorker(index: Int, schedule: MutableList<WorkSchedule>) {
+        for (i in index..<schedule.size) {
+            if (PublicHoliday.isHoliday(schedule[i].month, schedule[i].day) ||
+                Weekend.isWeekend(schedule[i].dayOfWeek)
+            ) {
+                val temp = schedule[index].name
+                schedule[index].name = schedule[i].name
+                schedule[i].name = temp
+                return
+            }
+        }
+    }
+
+    private fun findNextWeekdayWorker(index: Int, schedule: MutableList<WorkSchedule>) {
+        for (i in index..<schedule.size) {
+            if (!PublicHoliday.isHoliday(schedule[i].month, schedule[i].day) &&
+                !Weekend.isWeekend(schedule[i].dayOfWeek)
+            ) {
+                val temp = schedule[index].name
+                schedule[index].name = schedule[i].name
+                schedule[i].name = temp
+                return
+            }
+        }
     }
 }
