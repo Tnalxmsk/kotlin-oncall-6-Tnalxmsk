ffed13ad390d852bd3ea24c3ae2247f4b3bcbc3b
Tnalxmsk <alstnwkd990@naver.com>
2023-12-16 16:18:02 +0900

    feat(oncall): 주말인지 확인하는 기능 추가

--- src/main/kotlin/oncall/model/date/Weekend.kt
+++ src/main/kotlin/oncall/model/date/Weekend.kt
@@ -2,5 +2,16 @@ package oncall.model.date
 
 enum class Weekend(val day: String) {
     SATURDAY("토"),
-    SUNDAY("일"),
+    SUNDAY("일");
+
+    companion object {
+        fun isWeekend(inputDay: String): Boolean {
+            entries.forEach {
+                if (it.day == inputDay) {
+                    return true
+                }
+            }
+            return false
+        }
+    }
 }
