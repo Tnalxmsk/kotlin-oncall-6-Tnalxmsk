17aae952d1f49ef6d18881fc81229ff0d3cd04c9
Tnalxmsk <alstnwkd990@naver.com>
2023-12-16 16:41:18 +0900

    feat(oncall): 근무표를 출력하는 함수 추가

--- src/main/kotlin/oncall/view/OutputView.kt
+++ src/main/kotlin/oncall/view/OutputView.kt
@@ -1,4 +1,15 @@
 package oncall.view
 
+import oncall.model.WorkSchedule
+
 class OutputView {
+    fun printWorkSchedule(workSchedule: List<WorkSchedule>) {
+        workSchedule.forEach {
+            println(OUTPUT_SCHEDULE_FORMAT.format(it.month, it.day, it.dayOfWeek, it.name))
+        }
+    }
+
+    companion object {
+        private const val OUTPUT_SCHEDULE_FORMAT = "%d월 %d일 %s %s"
+    }
 }
