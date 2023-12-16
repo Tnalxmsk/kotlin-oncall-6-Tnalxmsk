608c5f3966f252003a5c6ad86774cdd58f8aa1dc
Tnalxmsk <alstnwkd990@naver.com>
2023-12-16 15:09:52 +0900

    feat(oncall): 월 규칙 추가
    31일까지 있는 달, 30일까지 있는 달,
    28일 까지 있는 달,
    특정 달의 마지막 날짜를 반환하는 기능 추가

---
+++ src/main/kotlin/oncall/model/date/MonthRole.kt
@@ -0,0 +1,13 @@
+package oncall.model.date
+
+enum class MonthRole(val month: List<Int>, val days: Int) {
+    THIRTY_FIRST(listOf(1, 3, 5, 7, 8, 10, 12), 31),
+    THIRTIETH(listOf(4, 6, 9, 11), 30),
+    TWENTY_EIGHTH(listOf(2), 28);
+
+    companion object {
+        fun getDays(month: Int): MonthRole {
+            return MonthRole.entries.find { it.month.contains(month) }!!
+        }
+    }
+}
