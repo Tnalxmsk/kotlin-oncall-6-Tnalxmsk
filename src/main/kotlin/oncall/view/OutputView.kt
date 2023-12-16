1038939e132e667e58343ee93625674d9d58783d
Tnalxmsk <alstnwkd990@naver.com>
2023-12-16 18:22:29 +0900

    fix(oncall): 공휴일이라면 특정 문자를 붙여서 출력하도록 수정

--- src/main/kotlin/oncall/view/OutputView.kt
+++ src/main/kotlin/oncall/view/OutputView.kt
@@ -1,15 +1,21 @@
 package oncall.view
 
+import oncall.model.date.PublicHoliday
 import oncall.model.schedule.WorkSchedule
 
 class OutputView {
     fun printWorkSchedule(workSchedule: List<WorkSchedule>) {
+        println()
         workSchedule.forEach {
-            println(OUTPUT_SCHEDULE_FORMAT.format(it.month, it.day, it.dayOfWeek, it.name))
+            if (PublicHoliday.isHoliday(it.month, it.day)) {
+                println(OUTPUT_HOLIDAY_FORMAT.format(it.month, it.day, it.dayOfWeek, it.name))
+
+            } else println(OUTPUT_SCHEDULE_FORMAT.format(it.month, it.day, it.dayOfWeek, it.name))
         }
     }
 
     companion object {
+        private const val OUTPUT_HOLIDAY_FORMAT = "%d월 %d일 %s(휴일) %s"
         private const val OUTPUT_SCHEDULE_FORMAT = "%d월 %d일 %s %s"
     }
 }
