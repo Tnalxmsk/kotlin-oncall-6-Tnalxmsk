8eb2c1760bd560f47e9278a548df0f2328f21ded
Tnalxmsk <alstnwkd990@naver.com>
2023-12-16 16:41:03 +0900

    feat(oncall): 현재 인덱스와 마지막 인덱스를 비교하여 초기화하는 함수 추가

--- src/main/kotlin/oncall/model/WorkScheduleGenerator.kt
+++ src/main/kotlin/oncall/model/WorkScheduleGenerator.kt
@@ -11,32 +11,39 @@ object WorkScheduleGenerator {
         val month = workDate.month
         val schedule = mutableListOf<WorkSchedule>()
         val days = MonthRole.getDays(month)
-
         var weekdayWorkerIndex = 0
         var weekendWorkerIndex = 0
         var dayOfWeekIndex = DayOfWeek.DAYS.day.indexOf(workDate.firstDayWeek)
 
         for (i in 1..days) {
             var worker: Worker
-            if (dayOfWeekIndex == DayOfWeek.DAYS.day.lastIndex) {
-                dayOfWeekIndex = 0
-            }
+            dayOfWeekIndex = checkDayOfWeekIndex(dayOfWeekIndex)
             val dayOfWeek = DayOfWeek.DAYS.day[dayOfWeekIndex]
             when {
                 PublicHoliday.isHoliday(month ,i) || Weekend.isWeekend(dayOfWeek) -> {
+                    weekendWorkerIndex = checkIndex(weekendWorkerIndex, weekendWorkers)
                     worker = weekendWorkers[weekendWorkerIndex]
                     weekendWorkerIndex++
                     dayOfWeekIndex++
-                    schedule.add(WorkSchedule(month, i, worker.name))
+                    schedule.add(WorkSchedule(month, i, dayOfWeek, worker.name))
                 }
                 else -> {
+                    weekdayWorkerIndex = checkIndex(weekdayWorkerIndex, weekDayWorkers)
                     worker = weekDayWorkers[weekdayWorkerIndex]
                     weekdayWorkerIndex++
                     dayOfWeekIndex++
-                    schedule.add(WorkSchedule(month, i, worker.name))
+                    schedule.add(WorkSchedule(month, i, dayOfWeek, worker.name))
                 }
             }
         }
         return schedule
+    }
+
+    private fun checkIndex(index: Int, workers: List<Worker>): Int {
+        return if (index > workers.lastIndex) 0 else index
+    }
+
+    private fun checkDayOfWeekIndex(index: Int): Int {
+        return if (index > DayOfWeek.DAYS.day.lastIndex) 0 else index
     }
 }
