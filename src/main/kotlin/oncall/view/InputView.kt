304b0e2f68712155966d13bf46e82262a60a4cc8
Tnalxmsk <alstnwkd990@naver.com>
2023-12-16 14:29:17 +0900

    feat(oncall): 평일 근무자 입력 기능 추가

--- src/main/kotlin/oncall/view/InputView.kt
+++ src/main/kotlin/oncall/view/InputView.kt
@@ -7,10 +7,18 @@ class InputView {
         print(INPUT_DATE_MESSAGE)
         val input = Console.readLine()
 
-        return input.split(",")
+        return input.split(SPLIT_DELIMITERS)
+    }
+
+    fun readWeekdayWorkerName(): List<String> {
+        print(INPUT_WEEKDAY_WORKER_NAME_MESSAGE)
+        val input = Console.readLine()
+        return input.split(SPLIT_DELIMITERS)
     }
 
     companion object {
+        private const val SPLIT_DELIMITERS = ","
         private const val INPUT_DATE_MESSAGE = "비상 근무를 배정할 월과 시작 요일을 입력하세요> "
+        private const val INPUT_WEEKDAY_WORKER_NAME_MESSAGE = "평일 비상 근무 순번대로 사원 닉네임을 입력하세요> "
     }
 }
